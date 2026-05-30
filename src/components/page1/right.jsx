95a939cd1f9f620b376d901b78f8038495fd5f0f
fatima muzafar <fatimamuzafar39@gmail.com>
2026-05-30 18:15:35 +0500

    button fixes

--- src/components/page1/right.jsx
+++ src/components/page1/right.jsx
@@ -50,7 +50,7 @@ function Right() {
         </h1>
       </div>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full items-stretch">
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 w-full [&>*]:h-full">
         {userdata.map(function (elem, index) {
           return (
             <Card
